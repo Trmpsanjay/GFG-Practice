f945b6bc3092133077e454676868b0be6ff15ca6
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-08-17 22:27:09 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,13 @@
+class Geeks {
+    // 
+	 static void evenOdd (int a,int b) 
+	 {
+		if(a%2==0){
+		    System.out.println(a+"\n"+b);
+		}
+		else{
+		    System.out.println(b+"\n"+a);
+		}
+	     
+	 }
+}
