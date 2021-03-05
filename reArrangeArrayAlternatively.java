60e335cd22167544d8195eedea48e98f9603e20a
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2021-03-05 11:39:28 +0530

    Create reArrangeArrayAlternatively.java

---
+++ reArrangeArrayAlternatively.java
@@ -0,0 +1,81 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    int[] temp= new int[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        temp[i] = Integer.parseInt(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(temp, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(temp[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(int arr[], int n){
+        
+        int i =0;
+        int j = arr.length-1;
+        int arrNew[] = new int[arr.length];
+        int k = 0;
+        int count = n;
+        while(k<count){
+            arrNew[k++] = arr[j--];
+            if(k<=arr.length-1)
+                arrNew[k++] = arr[i++];
+            
+        }
+        for(int l=0;l<arr.length;l++){
+            arr[l] = arrNew[l];
+        }
+        
+        
+        
+    }
+    
+}
+
+
